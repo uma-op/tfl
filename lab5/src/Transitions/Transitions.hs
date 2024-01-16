2e11c259005ade099d05d52df7d6b640732a6908
Arseniy Bannikov <bannikovtop@gmail.com>
2024-01-16 20:54:52 +0300

    Goto and closure functions

--- lab5/src/Transitions/Transitions.hs
+++ lab5/src/Transitions/Transitions.hs
@@ -4,6 +4,10 @@ module Transitions where
 import qualified Data.Either as Either
 import qualified Data.List as List
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Function as Function
+import qualified Data.Bifunctor as Bifunctor
+import qualified Data.Maybe as Maybe
 
 import qualified Grammar
 
@@ -14,19 +18,77 @@ data Situation =
         { symbol :: Grammar.Symbol
         , beforeDot :: [Grammar.Symbol]
         , afterDot :: [Grammar.Symbol] }
+    deriving (Eq, Show)
+
+instance Ord Situation where
+    compare x y =
+        compare
+            (symbol x, beforeDot x, afterDot x)
+            (symbol y, beforeDot y, afterDot y)
+
+nextSituation :: Situation -> Maybe Situation
+nextSituation (Situation { afterDot = [] }) = Nothing
+nextSituation s = Just $ s
+    { beforeDot = head (afterDot s) : beforeDot s
+    , afterDot = tail (afterDot s) }
+
+situationsFromGrammarRule :: Grammar.GrammarRule -> Set.Set Situation
+situationsFromGrammarRule rule =
+    Set.fromList $
+    List.zipWith
+        (\a b -> Situation
+            { symbol = Grammar.lhs rule
+            , beforeDot = List.reverse a
+            , afterDot = b } )
+        (List.inits $ Grammar.rhs rule)
+        (List.tails $ Grammar.rhs rule)
+
+startSituationFromGrammarRule :: Grammar.GrammarRule -> Situation
+startSituationFromGrammarRule rule =
+    Situation
+        { symbol = Grammar.lhs rule
+        , beforeDot = []
+        , afterDot = Grammar.rhs rule }
+
+finalSituationFromGrammarRule :: Grammar.GrammarRule -> Situation
+finalSituationFromGrammarRule rule =
+    Situation
+        { symbol = Grammar.lhs rule
+        , beforeDot = List.reverse $ Grammar.rhs rule
+        , afterDot = [] }
 
 newtype Transitions = Transitions { table :: Map.Map Int (Map.Map Grammar.Symbol Int, Map.Map Grammar.Symbol Action) }
 
-closure :: [Situation] -> [Situation] -> [Situation]
-closure = undefined
+closure :: Set.Set Situation -> Set.Set Grammar.GrammarRule -> Set.Set Situation
+closure ss rs =
+    Set.fold
+        (Set.union . closure')
+        ss ss
+    where
+        closure' (Situation { afterDot = (Grammar.NTerm nt:_) }) =
+            Set.map
+                startSituationFromGrammarRule
+                (Set.filter ((== Grammar.NTerm nt) . Grammar.lhs) rs)
+        closure' s = Set.empty
 
-goto :: Situation -> Grammar.Symbol -> Maybe Situation
-goto  = undefined
+goto ::
+    Set.Set Situation ->            -- set of 'from' situations
+    Grammar.Symbol ->               -- transition symbol
+    Set.Set Grammar.GrammarRule ->  -- rules
+    Set.Set Situation               -- set of 'to' situations
+goto ss ts =
+    closure
+        (Set.map
+            (Maybe.fromJust . nextSituation)
+            (Set.filter filterTransitable ss))
+    where
+        filterTransitable (Situation { afterDot = (ts':_) }) = ts' == ts
+        filterTransitable _ = False
 
 first rules =
     first' $
     Map.fromList $
-    rules >>= (\r -> (Grammar.lhs r, []) : List.map (\s -> if Grammar.isTerm s then (s, [s]) else (s, [])) (Grammar.rhs r) )
+    rules >>= (\r -> (Grammar.lhs r, Set.empty) : List.map (\s -> if Grammar.isTerm s then (s, Set.singleton s) else (s, Set.empty)) (Grammar.rhs r) )
     where
         first' result =
             case List.foldr firstOne (Either.Left result) rules of
@@ -34,19 +96,19 @@ first rules =
                 Either.Right newResult -> first' newResult
             where
                 firstOne rule result =
-                    if List.null newTerms then
+                    if Set.null newTerms then
                         result
                     else
-                        Either.Right (Map.update (Just . (newTerms ++)) (Grammar.lhs rule) unpackedResult)
+                        Either.Right (Map.update (Just . Set.union newTerms) (Grammar.lhs rule) unpackedResult)
                     where
                         unpackedResult = Either.fromLeft (Either.fromRight undefined result) result
                         rhsFirst = head $ Grammar.rhs rule
-                        newTerms = (unpackedResult Map.! rhsFirst) List.\\ (unpackedResult Map.! Grammar.lhs rule)
+                        newTerms = (unpackedResult Map.! rhsFirst) Set.\\ (unpackedResult Map.! Grammar.lhs rule)
 
 follow rules =
     follow' $
     Map.fromList $
-    rules >>= (\r -> (Grammar.lhs r, []) : List.map (, []) (Grammar.rhs r) )
+    rules >>= (\r -> (Grammar.lhs r, Set.empty) : List.map (, Set.empty) (Grammar.rhs r) )
     where
         first' = first rules
 
@@ -62,15 +124,15 @@ follow rules =
                     else
                         followOne
                             (rule { Grammar.rhs = tail $ Grammar.rhs rule })
-                            (Either.Right (Map.update (Just . (newTerms ++)) rhsFirst unpackedResult))
+                            (Either.Right (Map.update (Just . Set.union newTerms) rhsFirst unpackedResult))
                     where
                         unpackedResult = Either.fromLeft (Either.fromRight undefined result) result
                         rhsFirst = head $ Grammar.rhs rule
-                        newTerms = newTerms' List.\\ (unpackedResult Map.! rhsFirst)
+                        newTerms = newTerms' Set.\\ (unpackedResult Map.! rhsFirst)
                             where
                                 newTerms' =
                                     case Grammar.rhs rule of
-                                        [Grammar.End] -> []
+                                        [Grammar.End] -> Set.empty
                                         [_] -> unpackedResult Map.! Grammar.lhs rule
                                         (_ : s : _) -> first' Map.! s
 
--- lab5/testsuite/tests/TransitionsTest.hs
+++ lab5/testsuite/tests/TransitionsTest.hs
@@ -6,6 +6,7 @@ import Paths_lab5
 import Test.Framework.Providers.HUnit (testCase)
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import Transitions
 import Grammar
@@ -47,54 +48,91 @@ test_follow filename expected = predicate @? ""
                     print followed
                     return False
 
+test_goto filename expected ss ts = predicate @? ""
+    where
+        predicate = assertionPredicate $
+            do
+                (Right parsed) <- parseFromFile filename
+                let shaped = shapeGrammarRules parsed 
+                let cleaned = cleanGrammarRules (head shaped) (tail shaped)
+                let gotoed = goto ss ts (Set.fromList cleaned)
+                if gotoed == expected then do
+                    return True
+                else do
+                    putStrLn "Goto failed"
+                    print gotoed
+                    return False
+
 tests = 
     [ testGroup "First test"
         [ testCase "first clear" $
             test_first "testsuite/data/clear-grammar.txt" $
                 Map.fromList
-                    [ ( NTerm "S", [Term "a"] )
-                    , ( NTerm "A", [Term "a"] )
-                    , ( NTerm "B", [Term "b"] )
-                    , ( Term "a", [Term "a"] )
-                    , ( Term "b", [Term "b"] )
-                    , ( End, [End] ) ]
+                    [ ( NTerm "S", Set.singleton (Term "a") )
+                    , ( NTerm "A", Set.singleton (Term "a") )
+                    , ( NTerm "B", Set.singleton (Term "b") )
+                    , ( Term "a", Set.singleton (Term "a") )
+                    , ( Term "b", Set.singleton (Term "b") )
+                    , ( End, Set.singleton End ) ]
         , testCase "first dirty" $
             test_first "testsuite/data/dirty-grammar.txt" $
                 Map.fromList
-                    [ ( NTerm "S", [Term "a"] )
-                    , ( NTerm "A", [Term "d"] )
-                    , ( Term "a", [Term "a"] )
-                    , ( Term "d", [Term "d"] )
-                    , ( End, [End] ) ] ]
+                    [ ( NTerm "S", Set.singleton (Term "a") )
+                    , ( NTerm "A", Set.singleton (Term "d") )
+                    , ( Term "a", Set.singleton (Term "a") )
+                    , ( Term "d", Set.singleton (Term "d") )
+                    , ( End, Set.singleton End ) ] ]
         , testCase "first tanya" $
             test_first "testsuite/data/tanya-grammar.txt" $
                 Map.fromList
-                    [ ( NTerm "S", [Term "c"] )
-                    , ( Term "c", [Term "c"] )
-                    , ( End, [End] ) ]
+                    [ ( NTerm "S", Set.singleton (Term "c") )
+                    , ( Term "c", Set.singleton (Term "c") )
+                    , ( End, Set.singleton End ) ]
     , testGroup "Follow test"
         [ testCase "follow clear" $
             test_follow "testsuite/data/clear-grammar.txt" $
                 Map.fromList
-                    [ ( NTerm "S", [] )
-                    , ( NTerm "A", [Term "b"] )
-                    , ( NTerm "B", [End] )
-                    , ( Term "a", [Term "b"] )
-                    , ( Term "b", [End] )
-                    , ( End, [] ) ]
+                    [ ( NTerm "S", Set.empty )
+                    , ( NTerm "A", Set.singleton (Term "b") )
+                    , ( NTerm "B", Set.singleton End )
+                    , ( Term "a", Set.singleton (Term "b") )
+                    , ( Term "b", Set.singleton End )
+                    , ( End, Set.empty ) ]
         , testCase "follow dirty" $
             test_follow "testsuite/data/dirty-grammar.txt" $
                 Map.fromList
-                    [ (NTerm "S", [] )
-                    , (NTerm "A", [End] )
-                    , (Term "a", [Term "d"] )
-                    , (Term "d", [End] )
-                    , (End, []) ] 
+                    [ (NTerm "S", Set.empty )
+                    , (NTerm "A", Set.singleton End )
+                    , (Term "a", Set.singleton (Term "d") )
+                    , (Term "d", Set.singleton End )
+                    , (End, Set.empty ) ] 
         , testCase "follow tanya" $
             test_follow "testsuite/data/tanya-grammar.txt" $
                 Map.fromList
-                    [ ( NTerm "S", [Term "c", End] )
-                    , ( Term "c", [End, Term "c"] )
-                    , ( End, [] ) ] ] ]
+                    [ ( NTerm "S", Set.fromList [Term "c", End] )
+                    , ( Term "c", Set.fromList [End, Term "c"] )
+                    , ( End, Set.empty ) ] ]
+    , testGroup "Goto test"
+        [ testCase "goto clear" $
+            test_goto "testsuite/data/clear-grammar.txt"
+                (Set.fromList
+                    [ Situation
+                        { symbol = NTerm "B"
+                        , beforeDot = []
+                        , afterDot = [Term "b"] }
+                    , Situation
+                        { symbol = NTerm "S"
+                        , beforeDot = [NTerm "A"]
+                        , afterDot = [NTerm "B", End] } ])
+                (Set.fromList
+                    [ Situation
+                        { symbol = NTerm "S"
+                        , beforeDot = []
+                        , afterDot = [Term "a", Term "b", End]}
+                    , Situation
+                        { symbol = NTerm "S"
+                        , beforeDot = []
+                        , afterDot = [NTerm "A", NTerm "B", End] }])
+                (NTerm "A") ] ]
 
 main = defaultMain tests
